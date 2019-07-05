908b51f8bbfc83db8b12af48eef238f178dbc7d3
ajithk444 <Rasalam@123>
2019-07-05 17:14:08 +0530

    Initial Commit

---
+++ Xamarin.XamlApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.XamlApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public const double AppBorderWidth = 3.0;
+        public static string LoggedInUserName = "rsak90";
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            btn_Login.Clicked += Handle_Clicked;
+            txt_Username.TextChanged += Handle_TextChanged;
+        }
+
+        void Handle_Clicked(Object sender, EventArgs e)
+        {
+            LoggedInUserName = "ajithk444";
+            txt_Username.Text = LoggedInUserName;
+            Debug.WriteLine("Clicked by : " + txt_Username.Text + " | "+btn_Login.BorderWidth);
+        }
+
+        void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            if (txt_Username.Text!=null && txt_Username.Text.Length > 0)
+            {
+                btn_Login.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ Xamarin.XamlApp/Shared/SharedResources.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Essentials;
+using System.Drawing;
+
+namespace Xamarin.XamlApp.Shared
+{
+    public static class SharedResources
+    {
+        public static Color ButtonBkColor
+        {
+            get { return Color.Red; }
+        }
+
+        public static Color ButtonTextColor
+        {
+            get { return Color.Yellow; }
+        }
+    }
+}
